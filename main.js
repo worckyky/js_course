a57fb41d04639af95d928c7425aea4eb6cbb016a
worckyky <mymailtolaaa@gmal.com>
2019-08-22 09:04:09 +0300

    мой не первый комит, но я просто забыл(

---
+++ main.js
@@ -0,0 +1,11 @@
+var	 money = 1,
+		income, 
+		addExpenses, 
+		deposit, 
+		mission, 
+		period;
+// Тут я ввожу переменные
+
+alert(прив);  
+/* Тут я вывожу одну переменную 
+Оставляя комментарий в 2 строки*/

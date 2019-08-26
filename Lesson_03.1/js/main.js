3a29dbe40d5010162af12556db72d4fad5add5bc
user <user@MacBook-Pro-user2.local>
2019-08-26 23:02:06 +0300

    Добавил усложненое задание

--- Lesson_03.1/js/main.js
+++ Lesson_03.1/js/main.js
@@ -1,11 +1,13 @@
-lang = String(prompt('Какой язык вы хотите использовать, чтобы отобразить дни недели?'));
+let lang = String(prompt('Какой язык вы хотите использовать, чтобы отобразить дни недели?'));
 
-// if (lang == 'ru') {
-// 	console.log(["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]);
-// } else if (lang == 'en') {
-// 	console.log(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
-// };
+// 1 Вариант
+if (lang == 'ru') {
+	console.log(["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]);
+} else if (lang == 'en') {
+	console.log(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+};
 
+// 2 Вариант
 switch (lang) {
 	case('ru'):
 		console.log(["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]);
@@ -13,3 +15,20 @@ switch (lang) {
 	case('en'):
 		console.log(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
 };
+
+// 3 Вариант
+let matrix = [
+  ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"],
+  ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+];
+Matrix_Up = (matrix[0].slice(0, 7));
+Matrix_Down = (matrix[1].slice(0, 7));
+let question = lang == "ru" ? Matrix_Up : lang == "en" ? Matrix_Down : alert('Ты не угадал :(') ;
+
+console.log(question);
+
+// Последнее задание
+let namePerson = String(prompt('Имя пользователя'));
+let result = namePerson == "Артем" ? "директор" : namePerson == "Максим" ? "преподоватеь" : "студент" ;
+
+console.log(result);

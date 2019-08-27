effae2409f75db2e383570737da446b816ff356f
user <user@MacBook-Pro-user2.local>
2019-08-27 22:47:48 +0300

    Добавил 4е задание и улучшил усложненое 3е задание

--- Lesson_03.1/js/main.js
+++ Lesson_03.1/js/main.js
@@ -2,33 +2,60 @@ let lang = String(prompt('Какой язык вы хотите использо
 
 // 1 Вариант
 if (lang == 'ru') {
-	console.log(["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]);
+	console.log(["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]);
 } else if (lang == 'en') {
-	console.log(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+	console.log(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
 };
 
 // 2 Вариант
 switch (lang) {
-	case('ru'):
-		console.log(["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]);
+	case ('ru'):
+		console.log(["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]);
 		break;
-	case('en'):
-		console.log(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+	case ('en'):
+		console.log(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
 };
 
 // 3 Вариант
+
 let matrix = [
-  ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"],
-  ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+	["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+	['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 ];
-Matrix_Up = (matrix[0].slice(0, 7));
-Matrix_Down = (matrix[1].slice(0, 7));
-let question = lang == "ru" ? Matrix_Up : lang == "en" ? Matrix_Down : alert('Ты не угадал :(') ;
+Matrix_Ru = (matrix[0].slice(0, 7));
+Matrix_En = (matrix[1].slice(0, 7));
+let question = lang == "ru" ? Matrix_Ru : lang == "en" ? Matrix_En : alert('Ты не угадал :(');
 
 console.log(question);
 
-// Последнее задание
+// 3.1 Вариант
+
+let data = {
+	ru: [
+		"Понедельник",
+		"Вторник",
+		"Среда",
+		"Четверг",
+		"Пятница",
+		"Суббота",
+		"Воскресенье"
+	],
+	en: [
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday', 'Sunday'
+	]
+};
+
+console.log(data[lang]);
+
+
+
+// Последнее задание 
 let namePerson = String(prompt('Имя пользователя'));
-let result = namePerson == "Артем" ? "директор" : namePerson == "Максим" ? "преподоватеь" : "студент" ;
+let result = namePerson == "Артем" ? "директор" : namePerson == "Максим" ? "преподоватеь" : "студент";
 
 console.log(result);
---
+++ Lesson_04/js/main.js
@@ -0,0 +1,99 @@
+let	money = Number(prompt('Ваш месячный доход?')),
+    income = 'Десигн, Банковский счет, Долги народа',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'), 
+    deposit = confirm('Есть ли у вас депозит в банке?'), 
+    mission = 100000,
+    period = 6;
+
+
+
+// Выводим тип данных переменных
+let showTypeOf = function(data){
+	console.log(typeof(data) + ' - ' + data);
+};
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+
+
+// Функция выводит сумму расходов
+let Sum = function getExpensesMonth(a, b, c) {
+	let Spliter = addExpenses.split(', ');
+			a = Number(Spliter[0]);
+			b = Number(Spliter[1]);
+			c = Number(Spliter[2]);
+			return a + b + c ;
+};
+console.log('Функция выводит сумму расходов - ' + Sum());
+
+
+
+// Функция выводит разницу между доходами и рассходами
+let accumulatedMonth = function getAccumulatedMonth(a, b) {
+	a = money;
+	b = Sum();
+	return a - b ;
+};
+console.log('Функция выводит разницу между доходами и рассходами - ' + accumulatedMonth());
+
+
+
+// Функция выводит колличество месяцев за которое будет достигнута цель
+let targetMonth = function getTargetMonth(a, b) {
+	a = mission;
+	b = accumulatedMonth();
+	return Math.floor(a / b);
+};
+console.log('Функция выводит колличество месяцев (достижение цели) - ' + targetMonth());
+
+
+
+let Regress_One = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+		Howmuch_One = Number(prompt('Во сколько это обойдется?')),
+		Regress_Two = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+		Howmuch_Two = Number(prompt('Во сколько это обойдется?'));
+
+
+
+// Выводит чистый бюджет в месяц
+let budgetMonth = function(a, b, c){
+	a = money;
+	b = Howmuch_One;
+	c = Howmuch_Two;
+	return a - (b + c);
+};
+// Выводит бюджет в день
+let getBudgetDay = function(a, b) {
+	a = budgetMonth();
+	b = 30;
+	return Math.floor(a / b);
+};
+
+console.log('Выводит чистый бюджет в месяц - ' + budgetMonth());
+console.log('Выводит бюджет в день - ' + getBudgetDay());
+
+// let budgetMonth = money - (Howmuch_One + Howmuch_Two);
+// console.log(budgetMonth);
+
+// console.log(Math.ceil(mission / budgetMonth));
+
+// let budgetDay = Math.floor(money / 30);
+// console.log(budgetDay);
+
+
+
+
+// Определяем уровень дохода
+let getStatusIncome = function() {
+	if (getBudgetDay() >= 800) {
+		return('Высокий уровень дохода');
+	} else if (getBudgetDay() >= 300 && getBudgetDay() < 800) {
+		return('Средний уровень дохода');
+	} else if (getBudgetDay() > 0 && getBudgetDay() < 300) {
+		return('Средний уровень дохода');
+	} else if (getBudgetDay() <= 0) {
+		return('Что то пошло не так');
+	}
+};
+console.log('Определяем уровень дохода - ' + getStatusIncome());
